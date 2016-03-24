921ed6ac0c18b1a1bf83616f2f22dd7f36ef3511
Gregory Jansen <gregory.jansen@gmail.com>
2016-03-24 14:55:51 -0400

    putting the filesize in a dcterms:extent

--- siegfried.py
+++ siegfried.py
@@ -44,7 +44,7 @@ def process_file(parameters):
     afile = result['files'][0]  # always one file only
 
     content = {}  # assertions about the file
-    content['filesize'] = afile['filesize']
+    content['dcterms:extent'] = afile['filesize']
 
     matches = []
     for match in afile['matches']:
